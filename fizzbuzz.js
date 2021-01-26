7c28c68d7c93fd520631270b9b74c09d2860ca0a
Alexis Borron <elearnlex@gmail.com>
2021-01-26 12:45:33 -0600

    Second attempt at FizzBuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,12 +1,11 @@
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log('FizzBuzz');
-    } else if (i % 3 === 0) {
-        console.log('Fizz');
-    } else if (i % 5 === 0) {
-        console.log('Buzz');
-    } else {
-        console.log(i);
+for (let i = 1; i < 101; i++) {
+    let word = "";
+    if (i % 3 === 0) {
+        word += "Fizz";
     }
+    if (i % 5 === 0) {
+        word += "Buzz";
+    }
+    console.log(word || i)
 }
 
