d6814c787f2e29ca257c35e0aa82ef9ee25e77cd
Alexis Borron <elearnlex@gmail.com>
2021-02-02 18:29:56 -0600

    Updated solution with comments

--- minimum.js
+++ minimum.js
@@ -1,4 +1,8 @@
 let min = function(x,y) { // Defines min to hold function value that takes two arguments
-    return Math.min(x,y) // Returns minimum using Math.min function
-}
+    if (x < y) { // If x is less than y
+        return x; // Return the minimum, x
+    } else { // If x is not less than y 
+        return y; // Return the minimum, y
+    }
+    }
 
